7959c86cf067e30b326a40476fc6aacb5b8be909
Phạm Ngọc Nghĩa <lazydize130@gmail.com>
2019-10-01 01:54:10 +0700

    a

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,5 +12,56 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("hello github");
         }
+		public static bool IsOdd(int n)
+		{
+			if (n % 2 == 0)
+				return false;
+			else
+				return true;
+		}
+        public static bool IsEvent(int n)
+        {
+            if (n % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+            for (int i = 2; i < n; i++)
+            { 
+                    if (n % i == 0)
+                        return false;
+            }
+            return true;
+        }
+        public static int Square(int n)
+        {
+            //handle negative input
+            if (n < 0)
+                n = -n;
+            // Initialize result 
+            int y = n;
+            // Add n to res n-1 times 
+            for (int i = 1; i < n; i++)
+                y = n*n;
+                    return y;
+        }
+        public static int Cube(int n)
+        {
+            if (n < 0)
+                n = -n;
+            int y = n;
+            for (int i = 1; i < n; i++)
+                y = n*n*n;
+            return y;
+
+        } 
+        public static int pow(int x, int y)
+        {
+
+        }
     }
 }
